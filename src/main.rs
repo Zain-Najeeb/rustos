caa8d08306e781a89b8d2908b0fb0892e29922ad
--replace-all <--replace-all>
2025-04-18 20:17:42 -0400

    println! macro implemented

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{_info}");
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
     loop {}
 }
